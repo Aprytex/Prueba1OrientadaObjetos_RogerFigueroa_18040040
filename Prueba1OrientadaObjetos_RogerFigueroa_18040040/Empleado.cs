8609aa8297b3ca5980d1234b13ff7750b7add495
Aprytex <themaxrex9@gmail.com>
2023-11-11 22:49:05 -0600

    Initial commit

---
+++ Prueba1OrientadaObjetos_RogerFigueroa_18040040/Empleado.cs
@@ -0,0 +1,112 @@
+﻿public class Empleado
+{
+    public string Cedula { get; set; }
+    public string Nombre { get; set; }
+    public bool Activo { get; set; }
+
+    public Empleado() 
+    {
+        Cedula = "";
+        Nombre = "";
+        Activo = true;
+    }
+
+    public Empleado(string cedula, string nombre, bool activo) 
+    {
+        Cedula = cedula;
+        Nombre = nombre;
+        Activo = activo;
+    }
+
+    public virtual double CalcularSalario()
+    {
+
+        return 0.0;
+    }
+
+    public override string ToString()
+    {
+        return $"Cédula: {Cedula}, Nombre: {Nombre}, Activo: {(Activo ? "Si" : "No")}";
+    }
+
+    public class EmpleadoAsalariado : Empleado
+    {
+        public double SalarioMensual { get; set; }
+
+        public EmpleadoAsalariado()
+        {
+            SalarioMensual = 0.0;
+        }
+
+        public EmpleadoAsalariado(string cedula, string nombre, bool activo, double salarioMensual) : base(cedula, nombre, activo) 
+        {
+            SalarioMensual = salarioMensual;
+        }
+
+        public override double CalcularSalario()
+        {
+            return SalarioMensual;
+        }
+
+        public override string ToString()
+        {
+            return $"Tipo: Asalariado, {base.ToString()}, Salario Mensual: {SalarioMensual:C}";
+        }
+    }
+
+    public class EmpleadoPorHora : Empleado
+    {
+        public double PrecioHora { get; set; }
+        public int HorasTrabajadas { get; set; }
+
+        public EmpleadoPorHora()
+        {
+            PrecioHora = 0.0;
+            HorasTrabajadas = 0;
+        }
+
+        public EmpleadoPorHora(string cedula, string nombre, bool activo, double precioHora, int horasTrabajadas) : base(cedula, nombre, activo) 
+        {
+            PrecioHora = precioHora;
+            HorasTrabajadas = horasTrabajadas;
+        }
+
+        public override double CalcularSalario()
+        {
+            return PrecioHora * HorasTrabajadas;
+        }
+
+        public override string ToString()
+        {
+            return $"Tipo: Por Hora, {base.ToString()}, Precio por Hora: {PrecioHora:C}, Horas Trabajadas: {HorasTrabajadas}, Salario: {CalcularSalario():C}";
+        }
+    }
+
+    public class EmpleadoPorComision : Empleado
+    {
+        public double VentasMensuales { get; set; }
+        public double PorcentajeComision { get; set; }
+
+        public EmpleadoPorComision()
+        {
+            VentasMensuales = 0.0;
+            PorcentajeComision = 0.0;
+        }
+
+        public EmpleadoPorComision(string cedula, string nombre, bool activo, double ventasMensuales, double porcentajeComision) : base(cedula, nombre, activo)
+        {
+            VentasMensuales = ventasMensuales;
+            PorcentajeComision = porcentajeComision;
+        }
+
+        public override double CalcularSalario()
+        {
+            return VentasMensuales * (PorcentajeComision / 100);
+        }
+
+        public override string ToString()
+        {
+            return $"Tipo: Por Comisión, {base.ToString()}, Ventas Mensuales: {VentasMensuales:C}, Porcentaje de Comisión: {PorcentajeComision}%, Salario: {CalcularSalario():C}";
+        }
+    }
+}
---
+++ Prueba1OrientadaObjetos_RogerFigueroa_18040040/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static Empleado;
+
+class Program
+{
+    static List<Empleado> empleados = new List<Empleado>();
+
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Menú:");
+            Console.WriteLine("1. Agregar empleado");
+            Console.WriteLine("2. Calcular salario de empleado por cédula");
+            Console.WriteLine("3. Mostrar información de empleado por cédula");
+            Console.WriteLine("4. Mostrar todos los empleados");
+            Console.WriteLine("5. Salir");
+
+            int opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    AgregarEmpleado();
+                    break;
+                case 2:
+                    CalcularSalarioPorCedula();
+                    break;
+                case 3:
+                    MostrarInformacionPorCedula();
+                    break;
+                case 4:
+                    MostrarTodosLosEmpleados();
+                    break;
+                case 5:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida. Inténtelo de nuevo.");
+                    break;
+            }
+        }
+    }
+
+    static void AgregarEmpleado()
+    {
+        Console.WriteLine("Seleccione el tipo de empleado:");
+        Console.WriteLine("1. Asalariado");
+        Console.WriteLine("2. Por Hora");
+        Console.WriteLine("3. Por Comisión");
+
+        int tipoEmpleado = int.Parse(Console.ReadLine());
+
+        Console.Write("Cédula: ");
+        string cedula = Console.ReadLine();
+        Console.Write("Nombre: ");
+        string nombre = Console.ReadLine();
+        Console.Write("Activo (Si (Escribalo tal cual como se ve aqui) o No): ");
+        bool activo = Console.ReadLine().Equals("Si", StringComparison.OrdinalIgnoreCase);
+
+        switch (tipoEmpleado)
+        {
+            case 1:
+                Console.Write("Salario Mensual: ");
+                double salarioMensual = double.Parse(Console.ReadLine());
+                empleados.Add(new EmpleadoAsalariado(cedula, nombre, activo, salarioMensual));
+                break;
+            case 2:
+                Console.Write("Precio por Hora: ");
+                double precioHora = double.Parse(Console.ReadLine());
+                Console.Write("Horas Trabajadas: ");
+                int horasTrabajadas = int.Parse(Console.ReadLine());
+                empleados.Add(new EmpleadoPorHora(cedula, nombre, activo, precioHora, horasTrabajadas));
+                break;
+            case 3:
+                Console.Write("Ventas Mensuales: ");
+                double ventasMensuales = double.Parse(Console.ReadLine());
+                Console.Write("Porcentaje de Comisión: ");
+                double porcentajeComision = double.Parse(Console.ReadLine());
+                empleados.Add(new EmpleadoPorComision(cedula, nombre, activo, ventasMensuales, porcentajeComision));
+                break;
+            default:
+                Console.WriteLine("Tipo de empleado no válido.");
+                break;
+        }
+
+        Console.WriteLine("Empleado agregado con éxito.");
+    }
+
+    static void CalcularSalarioPorCedula()
+    {
+        Console.Write("Ingrese la cédula del empleado: ");
+        string cedula = Console.ReadLine();
+
+        foreach (var empleado in empleados)
+        {
+            if (empleado.Cedula == cedula)
+            {
+                Console.WriteLine($"Salario de {empleado.Nombre}: {empleado.CalcularSalario():C}");
+                return;
+            }
+        }
+
+        Console.WriteLine("Empleado no encontrado.");
+    }
+
+    static void MostrarInformacionPorCedula()
+    {
+        Console.Write("Ingrese la cédula del empleado: ");
+        string cedula = Console.ReadLine();
+
+        foreach (var empleado in empleados)
+        {
+            if (empleado.Cedula == cedula)
+            {
+                Console.WriteLine(empleado);
+                return;
+            }
+        }
+
+        Console.WriteLine("Empleado no encontrado.");
+    }
+
+    static void MostrarTodosLosEmpleados()
+    {
+        if (empleados.Count == 0)
+        {
+            Console.WriteLine("No hay empleados registrados.");
+        }
+        else
+        {
+            Console.WriteLine("Lista de Empleados:");
+            foreach (var empleado in empleados)
+            {
+                Console.WriteLine(empleado);
+            }
+        }
+    }
+}
